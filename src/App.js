9f2ed692deff09315d34515573b23cd7637242b2
Mechztcc <dojathan@gmail.com>
2020-09-15 16:31:09 -0300

    created a simple drag'n drop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 import GlobalStyle from './styles/global';
 
@@ -8,12 +10,12 @@ import Board from './components/Board';
 
 function App() {
   return (
-    <>
-    <Header />
-    <Board />
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
 
-    <GlobalStyle />
-    </>
+      <GlobalStyle />
+    </DndProvider>
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
+
+    const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'CARD'},
+    collect: monitor => ({
+        isDragging: monitor.isDragging(),
+    })
+})
+
     return (
-        <Container>
+        <Container ref={dragRef}>
             <header>
-                <Label color="#7159c1"/>
+                {data.labels.map(label => <Label key={label} color={label} />)}
             </header>
-            <p>Fazer porra nenhuma!</p>
+            <p>{data.content}</p>
             <img src="https://api.adorable.io/avatars/155/abott@adorable.png" alt=""></img>
         </Container>
     )
